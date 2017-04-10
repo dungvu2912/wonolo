6f803a8f9aa37bffa7a1f7ae46daf57bd78ee2e7
dungvu2912 <dungvu2912@gmail.com>
2017-04-10 07:04:41 +0700

    Add 2 methods compare_values, is_element_present

--- mypakage/base.py
+++ mypakage/base.py
@@ -4,15 +4,36 @@ import time
 
 
 class Page(object):
-	def __init__(self, driver, base_url='https://wonolo-dev.herokuapp.com/users/sign_in'):
-		self.base_url = base_url
-		self.driver = driver
-		self.timeout = 30
+    def __init__(self, driver, base_url='https://wonolo-dev.herokuapp.com/users/sign_in'):
+        self.base_url = base_url
+        self.driver = driver
+        self.timeout = 30
 
-	def find_element(self, *locator):
-		return self.driver.find_element(*locator)
+    #   Find the element by locator from locators.py
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
 
-	def wait_for_element(self, *locator):
-		return WebDriverWait(self, self.driver, 30).until(lambda self: self.find_element(*locator))
+    #   Wait until the element display
+    def wait_for_element(self, *locator):
+        return WebDriverWait(self.driver, 30).until(lambda self: self.find_element(*locator))
+
+    #   Pause for n seconds
+    def sleep(self, sleepTime):
+        time.sleep(sleepTime)
+
+    #   Compare between valueA and valueB
+    def compare_values(self, valueA, valueB):
+    	if(valueA == valueB):
+    		return True
+    	else:
+    		return False
+
+    #   Check if the element is present
+    def is_element_present(self,  *locator):
+        try: 
+        	self.driver.find_element(*locator)
+        except NoSuchElementException as e: 
+        	return False
+        return True
 
 
